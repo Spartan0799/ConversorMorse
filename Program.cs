d9e5a343f5a7f5769a498fc404cfaa6e0e2998db
Gabriel Santos <Spartan0799@users.noreply.github.com>
2016-09-27 23:24:32 -0300

    0.0.0

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Vocabulario
+            Dictionary<char, String> morseCode = new Dictionary<char, String>()
+            {
+                {'A' , ".-"},
+                { 'B' , "-..."},
+                { 'C' , "-.-."},
+                {'D' , "-.."},
+                { 'E' , "."},
+                { 'F' , "..-."},
+                {'G' , "--."},
+                { 'H' , "...."},
+                { 'I' , ".."},
+                {'J' , ".---"},
+                { 'K' , "-.-"},
+                { 'L' , ".-.."},
+                {'M' , "--"},
+                { 'N' , "-."},
+                { 'O' , "---"},
+                {'P' , ".--."},
+                { 'Q' , "--.-"},
+                { 'R' , ".-."},
+                {'S' , ".-."},
+                { 'T' , "-"},
+                { 'U' , "..-"},
+                {'V' , "...-"},
+                { 'W' , ".--"},
+                { 'X' , "-..-"},
+                {'Y' , "-.--"},
+                { 'Z' , "--.."},
+                {'0' , "-----"},
+                { '1' , ".----"},
+                { '2' , "..----"},
+                { '3' , "...--"},
+                {'4' , "....-"},
+                { '5' , "....."},
+                { '6' , "-...."},
+                { '7' , "--..."},
+                {'8' , "---.."},
+                { '9' , "----."},
+            };
+            #endregion
+
+            #region Conversao
+            Console.WriteLine("Datilografe o que deseja traduzir");
+            String input = Console.ReadLine();
+            input = input.ToUpper();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i > 0)
+                    Console.Write('/');
+
+                char c = input[i];
+                if (morseCode.ContainsKey(c))
+                    Console.Write(morseCode[c]);
+            }
+            #endregion
+
+            #region Frescura
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla...");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+    }
+}
